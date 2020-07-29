6bcb0f5d215be7b79bdacec23853c25544ca60c9
jtkoch <jtkoch12@gmail.com>
2020-07-28 20:27:30 -0500

    most functionality is good, still working out small bugs

--- game/src/App.js
+++ game/src/App.js
@@ -1,5 +1,6 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useCallback } from 'react'
 import Grid from './components/Grid'
+import './App.css'
 
 const startingRows = 30
 const startingColumns = 100
@@ -16,18 +17,20 @@ const gridDirections = [
 
 const emptyGrid = (numRows, numColumns) => {
     const rows = []
-    for (let i =0; i < numRows; i++) {
+    for (let i = 0; i < numRows; i++) {
         rows.push(Array(numColumns).fill(0))
     }
     return rows
 }
 
 const randomGrid = (numRows, numColumns) => {
-    const rows = []
-    for (let i = 0; i < numRows; i += 1) {
-        rows.push(Array.from(Array(numColumns), () => (Math.random() > 0.6 ? 1 : 0)))
-    }
-    return rows
+  const rows = []
+  for (let i = 0; i < numRows; i += 1) {
+    rows.push(
+      Array.from(Array(numColumns), () => (Math.random() > 0.6 ? 1 : 0))
+    )
+  }
+  return rows
 }
 
 const cellNeighbors = (grid, i, j) => {
@@ -46,14 +49,19 @@ const cellNeighbors = (grid, i, j) => {
 }
 
 
-
 function App() {
   const [grid, setGrid] = useState(() => {
     emptyGrid(startingRows, startingColumns)
   })
+
+  const [generation, setGeneration] = useState(0)
   const [running, setRunning] = useState(false)
-  const usingRef = useRef(running)
-  usingRef.current = running // updates the current value on every render
+  
+  const generationRef = useRef(generation)
+  generationRef.current = generation
+  
+  const runningRef = useRef(running)
+  runningRef.current = running // updates the current value on every render
 
   const onClickCellHandler = (rowIndex, columnIndex) => {
     setGrid(prevGrid => {
@@ -67,23 +75,57 @@ function App() {
     setRunning(!running)
 
     if (!running) {
-      usingRef.current = true
-      // runSimulation()
+      runningRef.current = true
+      runSimulation()
     }
   }
+
+    const runSimulation = useCallback(() => {
+      if (!runningRef.current) {
+        return
+      }
+
+      setGrid(prevGrid => {
+        const newGrid = prevGrid.map(rows => rows.slice(0));
+        
+        for (let i = 0; i < startingRows; i += 1) {
+          for (let j = 0; j < startingColumns; j += 1) {
+            const neighbors = cellNeighbors(prevGrid, i, j);
+            
+            if (neighbors < 2 || neighbors > 3) { // cell dies
+              newGrid[i][j] = 0;
+            } else if (newGrid[i][j] === 0 && neighbors === 3) { // cell lives
+              newGrid[i][j] = 1;
+            }
+          }
+        }
+        
+        return newGrid;
+      });
+  
+      setTimeout(runSimulation, 500);
+    }, []);
 
 
   return (
     <div>
-      <div>
+      <div className="generation">
+        Generation : {generation}
+      </div>
+      <div className="buttons">
         <button onClick={onStartHandler}>{`${running ? 'Stop' : 'Start'}`}</button>
         <button onClick={() => {
           setRunning(false)
           setGrid(emptyGrid(startingRows, startingColumns))
+          setGeneration(0)
         }}>Clear Grid</button>
-        <button onClick={() => setGrid(randomGrid(startingRows, startingColumns))}>Random Grid</button>
+        <button onClick={() => setGrid(randomGrid(startingRows, startingColumns))}>
+          Random Grid
+        </button>
       </div>
+      <div className="grid">
         <Grid grid={grid} onClick={onClickCellHandler} />
+      </div>
     </div>
   )
 }
--- game/src/components/Grid.js
+++ game/src/components/Grid.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Grid = () => {
+const GridContainer = styled.div`
+    display: grid;
+    grid-gap: 2px;
+    grid-template-columns: ${({ numColumns }) => (`repeat(${numColumns}, 20px)`)};
+    padding: 0 12px 12px;
+`
+
+const GridCell = styled.div`
+    grid-column-start: auto;
+    grid-row-start: auto;
+    border: 1px solid black;
+    cursor: pointer;
+    width: 20px;
+    height: 20px;
+    background-color: ${({ isAlive }) => (isAlive ? 'salmon' : 'transparent')};
+`
+
+const Grid = ({ grid, onClick }) => {
     return (
-        <div>
-            <h1>Grid</h1>
-        </div>
+        <GridContainer numColumns={grid.length}>
+            {grid.map((rows, rowIndex) => (
+                rows.map((column, columnIndex) => (
+                    <GridCell
+                        isAlive={column} // column value: 1 or 0
+                        key={`${rowIndex}_${columnIndex}`}
+                        onClick={() => onClick(rowIndex, columnIndex)}
+                    />
+                ))
+            ))}
+        </GridContainer> 
     )
 }
 
